89e9be21ce3869a5056a088929275d03dd1390c4
Alexander Rocha <alexrochalopez@gmail.com>
2025-07-26 21:03:21 -0400

    Initial Commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from '/src/assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+<>
+
+<Header />
+<Main />
+<Footer />
+<ModalWithForm />
+<ItemModal />
+{/* should be child of Main <WeatherCard />
+<ItemCard /> */}
+
+</>
+}
+
+export default App
